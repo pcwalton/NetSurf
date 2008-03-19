54ea5c8901a2142bc77837258c6113a9a36e6c2c
jmb <jmb@d004b94f-0d10-0410-95b5-d60e4a4a7a9a>
2008-03-19 21:21:48 +0000

    More comprehensive solution
    git-svn-id: svn://svn.netsurf-browser.org/trunk@3989 d004b94f-0d10-0410-95b5-d60e4a4a7a9a

--- backup/svn-backup.pl
+++ backup/svn-backup.pl
@@ -7,40 +7,60 @@ use constant REPOS_PATH => "/home/netsurf/svn";
 
 open LOG, ">backup.log" or die "failed to open backup.log: $!\n";
 
-# Get old revision
-my $old_revision = 0;
-foreach my $f (glob 'netsurf-svn-r*') {
-	($old_revision) = ($f =~ /^netsurf-svn-r(.*)\.gz/);
-}
+my @days = ( "sun", "mon", "tue", "wed", "thu", "fri", "sat" );
 
-# Calculate base for this dump
-my $base_revision = $old_revision + 1;
+# Day of the week [0,6] 0 == Sunday
+my $day = (command("date +'%w'"))[0];
+chomp $day;
 
 # Revision of current head
 my $cur_revision = (command("svnlook youngest " . REPOS_PATH))[0];
 chomp $cur_revision;
 
-# Don't do anything if there haven't been any commits since last time
-if ($cur_revision == $old_revision) {
-	print LOG "Nothing changed\n";
-	exit;
-}
+if ($day == 0 || ! -f "currev") {
+	# Perform complete backup
+	command("svnadmin dump " . REPOS_PATH .
+		" -r 0:$cur_revision --deltas" .
+		" >netsurf-svn-$days[$day]");
+} else {
+	# Incremental since yesterday, please
+	# Get yesterday's revision
+	my $old_revision = 0;
+	open REVFILE, "<currev" or die "failed to open currev: $!\n";
+	foreach my $line (<REVFILE>) {
+		$old_revision = $line;
+	}
+	close REVFILE;
+	chomp $old_revision;
 
-# Perform the dump
-command("svnadmin dump " . REPOS_PATH . " -r $base_revision:$cur_revision " .
-	"--incremental --deltas >>netsurf-svn");
+	# Calculate base for this dump
+	my $base_revision = $old_revision + 1;
+
+	if ($cur_revision == $old_revision) {
+		print LOG "Nothing changed\n";
+		# FIXME: what do we actually want to do here?
+		command("rm -f netsurf-svn-$days[$day]");
+		command("touch netsurf-svn-$days[$day]");
+	} else {
+		# Perform the dump
+		command("svnadmin dump " . REPOS_PATH . 
+			" -r $base_revision:$cur_revision " .
+			"--incremental --deltas >netsurf-svn-$days[$day]");
+	}
+}
 
 # Gzip it, in an rsync-friendly manner
-command("gzip -5 -c --rsyncable netsurf-svn >netsurf-svn-r$cur_revision.gz");
+command("gzip -5 -c --rsyncable netsurf-svn-$days[$day] " .
+	">netsurf-svn-$days[$day].gz");
 
 # Rsync it to batfish
-command("rsync --verbose --compress --times netsurf-svn-r$cur_revision.gz " .
+command("rsync --verbose --compress --times netsurf-svn-$days[$day].gz " .
 	"netsurf\@netsurf-browser.org:/home/netsurf/");
 
-# And remove the previous backup
-if ($old_revision > 0) {
-	command("rm -f netsurf-svn-r$old_revision.gz");
-}
+# Update current revision indicator
+open REVFILE, ">currev" or die "failed to open currev: $!\n";
+print REVFILE "$cur_revision\n";
+close REVFILE;
 
 sub command {
 	my $cmd = shift;
