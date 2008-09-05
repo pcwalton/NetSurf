27f4d1c380b3c51539b2e5b321f95bc3b687f0e4
rjek <rjek@d004b94f-0d10-0410-95b5-d60e4a4a7a9a>
2008-09-05 09:19:48 +0000

    Change libnsbmp to MIT licence
    git-svn-id: svn://svn.netsurf-browser.org/trunk@5242 d004b94f-0d10-0410-95b5-d60e4a4a7a9a

--- libnsbmp/libnsbmp.c
+++ libnsbmp/libnsbmp.c
@@ -1,20 +1,10 @@
 /*
- * Copyright 2006 Richard Wilson <info@tinct.net>
+ * Copyright 2006 Richard Wilson <richard.wilson@netsurf-browser.org>
  * Copyright 2008 Sean Fox <dyntryx@gmail.com>
  *
- * This file is part of NetSurf, http://www.netsurf-browser.org/
- *
- * NetSurf is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License.
- *
- * NetSurf is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * This file is part of NetSurf's libnsbmp, http://www.netsurf-browser.org/
+ * Licenced under the MIT License,
+ *                http://www.opensource.org/licenses/mit-license.php
  */
 
 #include <assert.h>
--- libnsbmp/utils/log.h
+++ libnsbmp/utils/log.h
@@ -1,27 +1,16 @@
 /*
  * Copyright 2003 James Bursa <bursa@users.sourceforge.net>
  * Copyright 2004 John Tytgat <John.Tytgat@aaug.net>
- * Copyright 2008 Sean Fox <dyntryx@gmail.com>
  *
  * This file is part of NetSurf, http://www.netsurf-browser.org/
- *
- * NetSurf is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License.
- *
- * NetSurf is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Licenced under the MIT License,
+ *                http://www.opensource.org/licenses/mit-license.php
  */
 
 #include <stdio.h>
 
-#ifndef _LIBNSBMP_LOG_H_
-#define _LIBNSBMP_LOG_H_
+#ifndef _LIBNSGIF_LOG_H_
+#define _LIBNSGIF_LOG_H_
 
 #ifdef NDEBUG
 #  define LOG(x) ((void) 0)
