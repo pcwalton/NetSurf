4849bd3ff1aa3136f385b64c0934829759cb61ae
jmb <jmb@d004b94f-0d10-0410-95b5-d60e4a4a7a9a>
2008-11-10 19:08:49 +0000

    Test driver for library initialisation/finalisation
    git-svn-id: svn://svn.netsurf-browser.org/trunk@5679 d004b94f-0d10-0410-95b5-d60e4a4a7a9a

---
+++ iconv/test/iconv.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include <iconv/iconv.h>
+
+#include "testutils.h"
+
+int main(int argc, char **argv)
+{
+	if (argc != 2) {
+		printf("Usage: %s <aliases_file>\n", argv[0]);
+		return 1;
+	}
+
+	assert(iconv_initialise(argv[1]) == 1);
+
+	iconv_finalise();
+
+	printf("PASS\n");
+
+	return 0;
+}
+
