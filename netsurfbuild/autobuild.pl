1cb9b81279b7d0e69fec561cc769ad4d2094d9e2
jmb <jmb@d004b94f-0d10-0410-95b5-d60e4a4a7a9a>
2009-02-02 00:59:59 +0000

    Explicitly state prefix.
    git-svn-id: svn://svn.netsurf-browser.org/trunk@6351 d004b94f-0d10-0410-95b5-d60e4a4a7a9a

--- netsurfbuild/autobuild.pl
+++ netsurfbuild/autobuild.pl
@@ -83,7 +83,7 @@ command("make TARGET=riscos install");
 
 chdir "$root/libharu";
 command("svn update --non-interactive");
-command("make TARGET=riscos");
+command("make TARGET=riscos PREFIX=/home/riscos/env");
 
 chdir "$root/libparserutils";
 command("svn update --non-interactive");
