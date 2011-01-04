c8131f24b708227f282ed288f6f3ab1483d3f655
jmb <jmb@d004b94f-0d10-0410-95b5-d60e4a4a7a9a>
2011-01-04 08:48:25 +0000

    Obtain Aliases file from the Unicode resource -- testrunner no longer passes it to us
    git-svn-id: svn://svn.netsurf-browser.org/trunk@11191 d004b94f-0d10-0410-95b5-d60e4a4a7a9a

--- iconv/test/iconv.c
+++ iconv/test/iconv.c
@@ -1,17 +1,45 @@
 #include <stdio.h>
+#include <string.h>
 
 #include <iconv-internal/iconv.h>
 
 #include "testutils.h"
 
+#ifdef __riscos__
+#define ALIASES_FILE "Files.Aliases"
+#else
+#define ALIASES_FILE "Files/Aliases"
+#endif
+
 int main(int argc, char **argv)
 {
-	if (argc != 2) {
-		printf("Usage: %s <aliases_file>\n", argv[0]);
-		return 1;
-	}
+	const char *ucpath;
+	int alen;
+	char aliases[4096];
 
-	assert(iconv_initialise(argv[1]) == 1);
+	UNUSED(argc);
+	UNUSED(argv);
+
+#ifdef __riscos__
+	ucpath = "Unicode:";
+#else
+	ucpath = getenv("UNICODE_DIR");
+#endif
+
+	assert(ucpath != NULL);
+
+	strncpy(aliases, ucpath, sizeof(aliases));
+	alen = strlen(aliases);
+#ifndef __riscos__
+	if (aliases[alen - 1] != '/') {
+		strncat(aliases, "/", sizeof(aliases) - alen - 1);
+		alen += 1;
+	}
+#endif
+	strncat(aliases, ALIASES_FILE, sizeof(aliases) - alen - 1);
+	aliases[sizeof(aliases) - 1] = '\0';
+
+	assert(iconv_initialise(aliases) == 1);
 
 	iconv_finalise();
 
